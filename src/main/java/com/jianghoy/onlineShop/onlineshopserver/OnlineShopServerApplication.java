44f78ff4f47871ac1a6fd57eaaf08fd513871a0a
Jianghong Ying <YJianghong@gmail.com>
2019-07-20 22:11:19 -0400

    first commit

---
+++ src/main/java/com/jianghoy/onlineShop/onlineshopserver/OnlineShopServerApplication.java
@@ -0,0 +1,13 @@
+package com.jianghoy.onlineShop.onlineshopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jianghoy/onlineShop/onlineshopserver/OnlineShopServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jianghoy.onlineShop.onlineshopserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineShopServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

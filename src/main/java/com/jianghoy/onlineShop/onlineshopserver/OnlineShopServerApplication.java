5e832d6b6e50e4c461662e2b0d1af05640332eae
Jianghong Ying <YJianghong@gmail.com>
2019-07-22 17:33:56 -0400

    update test

--- src/main/java/com/jianghoy/onlineShop/onlineshopserver/OnlineShopServerApplication.java
+++ src/main/java/com/jianghoy/onlineShop/onlineshopserver/OnlineShopServerApplication.java
@@ -2,8 +2,10 @@ package com.jianghoy.onlineShop.onlineshopserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({DataSourceConfig.class})
 public class OnlineShopServerApplication {
 
 	public static void main(String[] args) {
